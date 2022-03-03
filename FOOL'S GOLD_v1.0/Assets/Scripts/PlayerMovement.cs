c184118a699f77d05c3e1d29b0a5024cc3afcf93
Taylor Masiello <masiellotay@gmail.com>
2022-03-03 14:08:51 -0800

    Kenny (player) animations almost finished

---
+++ FOOL'S GOLD_v1.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Rigidbody2D playerRb;
+    [SerializeField] Animator animator;
+
+    public Vector3 playerWorldPos;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        CharMovement();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 playerWorldPos = playerRb.transform.position;
+
+        playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void CharMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
