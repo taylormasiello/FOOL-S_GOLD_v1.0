57cb03246070aa7328c0d8440508e1a8755b4972
Taylor Masiello <masiellotay@gmail.com>
2022-03-04 07:13:44 -0800

    Menus fully functional; HUD created

---
+++ FOOL'S GOLD_v1.0/Assets/Scripts/GameCursor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCursor : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Button backpackBtn;
+    [SerializeField] Texture2D searchingCursorTexture;
+    [SerializeField] Texture2D miningCursorTexture;
+    public bool isPickaxe;
+
+    void Start()
+    {
+        SetSearchingCursor(searchingCursorTexture);
+        isPickaxe = false;
+        backpackBtn.onClick.AddListener(TogglePickaxe);
+    }
+
+    public void SetSearchingCursor(Texture2D texture)
+    {
+        Cursor.SetCursor(searchingCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void SetMiningCursor(Texture2D texture)
+    {
+        Cursor.SetCursor(miningCursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void TogglePickaxe()
+    {
+        if (!isPickaxe)
+        {
+            isPickaxe = true;
+            SetMiningCursor(miningCursorTexture);
+        }
+        else if (isPickaxe)
+        {
+            isPickaxe = false;
+            SetSearchingCursor(searchingCursorTexture);
+        }
+    }
+}
