09c596115ca2fb30969ee5b6fa86c3fbae399fe0
Taylor Masiello <masiellotay@gmail.com>
2022-03-15 20:47:35 -0700

    fov bounds bug fix, mesh bounds set;

--- FOOL'S GOLD_v1.0/Assets/Scripts/FieldOfView.cs
+++ FOOL'S GOLD_v1.0/Assets/Scripts/FieldOfView.cs
@@ -17,7 +17,7 @@ public class FieldOfView : MonoBehaviour
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        fov = 45f;
+        fov = 60f;
         origin = Vector3.zero;
     }
 
@@ -74,6 +74,7 @@ public class FieldOfView : MonoBehaviour
         mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
     }
 
     public void SetOrigin(Vector3 origin)
